f5fb037f6a6f0ffa077241838872412f5b600d84
Romanjan-Das <romanjandas.mail@gmail.com>
2021-01-25 14:36:10 +0530

    basic form created

---
+++ temp_reg.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enlighten Career Institute : Best IIT JAM Coaching in Guwahati Register Page</title>
+    <style>
+        body{
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+        .main_content{
+            position: absolute;
+            padding: 0px;
+            margin: 0px;
+            left: 20vw;
+            top: 12vh;
+        }
+        #msg_span{
+            color: tomato;
+        }
+    </style>
+</head>
+<body>
+    <div class="main_content">
+    <form id="form_id" action="temp_save.php" method="post">
+        <label for="full_name">Enter your name</label><br>
+        <input type="text" name="full_name" id="full_name" placeholder="Full Name" required><br><br>
+        <label for="phone_number">Enter your phone number</label><br>
+        <input type="text" name="phone_number" id="phone_number" placeholder="phone number" required><br><br>
+        <label for="mail_address">Enter your email</label><br>
+        <input type="text" name="mail_address" id="mail_address" placeholder="xyz@example.com" required><br><br>
+        <label for="address">Enter your address</label><br>
+        <input type="text" name="address" id="address" placeholder="address" required>
+        <input type="text" name="url_address" id="url_address" value="<?php echo $_SERVER['HTTP_REFERER']; ?>">
+        <br><br>
+        <input type="submit" value="Submit">
+    </form>
+</div>
+</body>
+</html>
---
+++ temp_save.php
@@ -0,0 +1,24 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $file = fopen("reg_data.html","r");
+    $file_content = fread($file,filesize("reg_data.html"));
+    //echo  $file_content;
+    fclose($file);
+/*
+    $reg_form_data = "<br>" . "username: " . $_POST["username"] ."<br>" .  "fullname: " . $_POST["full_name"] ."<br>" .  "phone_number: " . 
+     $_POST["phone_number"] ."<br>" .  "mail_address: " . $_POST["mail_address"] ."<br>" .  "address: " . $_POST["address"] ."<br>" .  
+     "password: " . $_POST["password"];
+*/
+
+    $reg_form_data = $file_content . "<br>" . $_POST["full_name"] ."<br>" . 
+    $_POST["phone_number"] ."<br>" . $_POST["mail_address"] ."<br>" . $_POST["address"] .
+    "<br>____________________________<br>";
+
+    $file2 = fopen("reg_data.html","w");
+    fwrite($file2,$reg_form_data);
+    fclose($file2);
+
+    header('Location: ' . $_POST["url_address"]);
+}
+
+?>
